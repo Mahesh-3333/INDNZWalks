97c212956f5f995f0afdca8a574e79883d1ef11a
maheshdharme <mdharme33@gmail.com>
2023-01-18 22:08:09 +0530

    all changes done

---
+++ INDNZWalks/INDNZWalks.API/Data/INDNZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using INDNZWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace INDNZWalks.API.Data
+{
+    public class INDNZWalksDbContext : DbContext
+    {
+        public INDNZWalksDbContext(DbContextOptions<INDNZWalksDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ INDNZWalks/INDNZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INDNZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigation properies
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
